4cd88b18cd5d099313a219bd4a1b4127466e806a
AnshBalgotra <anshbalgotra@gmail.com>
2026-02-10 13:47:07 +0530

    first commit

---
+++ client/src/data/features.tsx
@@ -0,0 +1,20 @@
+import type { IFeature } from "../types";
+
+export const featuresData: IFeature[] = [
+    {
+        icon: "/assets/zap-icon.svg",
+        title: "Smart Analysis",
+        description: "Our AI analyzes your content to generate the most engaging thumbnail designs.",
+    },
+    {
+        icon: "/assets/thumb-icon.svg",
+        title: "Eye Catching Designs",
+        description: "Generate vibrant,high contrast thumbnails that grab attention instantly.",
+    },
+    {
+        icon: "/assets/shape-icon.svg",
+        title: "Fully Customizable",
+        description: "Get fully layered designs you can tweak to perfection if needed.",
+    },
+  
+];
